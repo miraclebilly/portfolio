299fdff074d3e28c2e614cb07ad2eb8fa4cd322f
Miracle Billy <uniquemiraculous@gmail.com>
2024-06-19 04:08:09 +0100

    type updated

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -32,8 +32,7 @@ export default function Form() {
                     <label htmlFor="message" className="block text-sm font-medium text-gray-700">Your Message</label>
                     <textarea 
                       name="message" 
-                      id="message" 
-                      rows="4" 
+                      id="message"    
                       className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" 
                       required
                     />
