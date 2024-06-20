cc88efb1906c745de93c734cd3cc55d2f34f735b
Miracle Billy <uniquemiraculous@gmail.com>
2024-06-20 01:47:04 +0100

    ui added

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -52,7 +52,6 @@ export default function Form() {
             <textarea
               name="message"
               id="message"
-              rows="4"
               className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
