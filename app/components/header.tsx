618fcc49c0b0ccef2794996e57606ecf66298f24
Miracle Billy <uniquemiraculous@gmail.com>
2024-06-25 00:52:44 +0100

    UI updated

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,8 +3,9 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 export default function Header() {
-    const cv = "https://media.licdn.com/dms/document/media/D4D2DAQFqJyYW4JmeOQ/profile-treasury-document-pdf-analyzed/0/1718725854619?e=1719446400&v=beta&t=aJnXBfFl7jVvu3YUrWgboqTUezlqa3SmRtKgOf2g3lw";
+    const cv = "https://drive.google.com/file/d/1t6RU7K0AdCebQ_lsrdUvoZp7Ewe1iHbu/view";
     const [darkMode, setDarkMode] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     
 
     useEffect(() => {
@@ -20,16 +21,20 @@ export default function Header() {
         return;
     }
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    };
+
     return (
         <header className="bg-white dark:bg-gray-900 shadow-md sticky top-0 z-50">
-            <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                <div className="text-2xl font-bold text-gray-900 dark:text-white">Miracle Billy</div>
-                <nav className="space-x-6">
-                    <a href="#" className="text-gray-700 dark:text-gray-300 hover:text-blue-500">Home</a>
+            <div className=" flex container mx-auto px-4 py-4 flex justify-between items-center">
+                <div className="text-2xl font-bold text-gray-900 dark:text-white"><Link href="/">Miracle Billy</Link></div>
+                <nav className="hidden md:flex lg:flex space-x-6">
+                    <Link href="#" className="text-gray-700 dark:text-gray-300 hover:text-blue-500">Home</Link>
                     <Link href="#section1" className="text-gray-700 dark:text-gray-300 hover:text-blue-500">Projects</Link>
                     <Link href="#section3" className="text-gray-700 dark:text-gray-300 hover:text-blue-500">About</Link>
-                    <a href="#section2" className="text-gray-700 dark:text-gray-300 hover:text-blue-500">Contact</a>
-                    <a href={cv} target="_blank" className="text-gray-700 dark:text-gray-300 hover:text-blue-500"><small>Resume</small></a>
+                    <Link href="#section2" className="text-gray-700 dark:text-gray-300 hover:text-blue-500">Contact</Link>
+                    <Link href={cv} target="_blank" className="text-gray-700 dark:text-gray-300 hover:text-blue-500"><small>Resume</small></Link>
                 </nav>
                 <button 
                     onClick={changeMode} 
@@ -37,7 +42,32 @@ export default function Header() {
                 >
                     {darkMode ? '☀️' : '🌙'}
                 </button>
+                <button
+                    onClick={toggleMenu}
+                    className="lg:hidden md:hidden items-center bg-gray-200 dark:bg-gray-700 text-gray-900 dark:text-gray-200 p-2 rounded-full"
+                >
+                    {isOpen ? '✖️' : '☰'}
+                </button>
             </div>
+            {isOpen && (
+                <div className="lg:hidden bg-white dark:bg-gray-900 shadow-md">
+                    <nav className="flex flex-col space-y-4 px-4 py-4">
+                    <Link href="#section1"
+                        className="text-gray-700 dark:text-gray-300 hover:text-blue-500" 
+                        onClick={toggleMenu}>Projects
+                    </Link>
+                    <Link href="#section3"
+                        className="text-gray-700 dark:text-gray-300 hover:text-blue-500" 
+                        onClick={toggleMenu}>
+                        About
+                        
+                    </Link >
+                    <Link href="#section2" className="text-gray-700 dark:text-gray-300 hover:text-blue-500" onClick={toggleMenu}>Contact</Link>
+                    <Link href={cv} target="_blank" className="text-gray-700 dark:text-gray-300 hover:text-blue-500" onClick={toggleMenu}><small>Resume</small></Link>
+                    </nav>
+
+                </div>
+            )}
         </header>
     );
 }
